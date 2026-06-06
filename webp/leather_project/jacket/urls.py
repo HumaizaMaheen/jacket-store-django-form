68aa025a97af9714ec032f2717f4527e30fa63fe
HumaizaMaheen <maheenhumaiza@gmail.com>
2026-06-06 06:55:25 +0500

    Delete webp/leather_project directory

--- webp/leather_project/jacket/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.addJacket),
-    path('show/', views.showAll),
-    path('delete', views.delete),
-    path('update', views.update),
-]
